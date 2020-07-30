0d2fc8fe7ccaa909469e57b5d4c9cde281594ff9
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2020-07-30 22:19:41 +0200

    update filtering

--- esp32/camAndFiltering/lib/filtering/filtering.h
+++ esp32/camAndFiltering/lib/filtering/filtering.h
@@ -6,8 +6,7 @@
 #include <array>
 #include <unordered_map>
 
-namespace filtering
-{
+namespace filtering {
 
 enum class EFilteringMethod {
     floydSteinberg,
@@ -21,50 +20,50 @@ enum class EFilteringMethod {
     stevensonArce
 };
 
-struct EnumClassHash
-{
-    template <typename T>
-    std::size_t operator()(T t) const
-    {
-        return static_cast<std::size_t>(t);
-    }
+template <typename T>
+inline T clamp(const T& value, const T& min, const T& max) { return std::max(min, std::min(max, value)); }
+
+struct PatternPart {
+    int deltaCols;
+    int deltaRows;
+    float proportion;
 };
 
-const std::array<std::pair<EFilteringMethod, std::vector<std::array<float, 3>>>, 9> diffusionPatterns {{
+const std::array<std::pair<EFilteringMethod, std::vector<PatternPart>>, 9> diffusionPatterns {{
     {EFilteringMethod::floydSteinberg,
-        { {1, 0,  7 / 16}, {-1, 1, 3 / 16}, {0, 1,  5 / 16}, {1, 1,  1 / 16} }
+        { {1, 0,  7.0f / 16.0f}, {-1, 1, 3.0f / 16.0f}, {0, 1,  5.0f / 16.0f}, {1, 1,  1.0f / 16.0f} }
     },
     {EFilteringMethod::atkinson,
-        { {1, 0,  1 / 8}, {2, 0,  1 / 8}, {-1, 1, 1 / 8}, {0, 1,  1 / 8}, {1, 1,  1 / 8}, {0, 2,  1 / 8} }
+        { {1, 0,  1.0f / 8.0f}, {2, 0,  1.0f / 8.0f}, {-1, 1, 1.0f / 8.0f}, {0, 1,  1.0f / 8.0f}, {1, 1,  1.0f / 8.0f}, {0, 2,  1.0f / 8.0f} }
     },
     {EFilteringMethod::jarvisJudiceNinke,
-        { {1, 0,  7 / 48}, {2, 0,  5 / 48}, {-2, 1, 3 / 48}, {-1, 1, 5 / 48}, {0, 1,  7 / 48}, {1, 1,  5 / 48}, {2, 1,  3 / 48}, {-2, 2, 1 / 48}, {-1, 2, 3 / 48}, {0, 2,  5 / 48}, {1, 2,  3 / 48}, {2, 2,  1 / 48} }
+        { {1, 0,  7.0f / 48.0f}, {2, 0,  5.0f / 48.0f}, {-2, 1, 3.0f / 48.0f}, {-1, 1, 5.0f / 48.0f}, {0, 1,  7.0f / 48.0f}, {1, 1,  5.0f / 48.0f}, {2, 1,  3.0f / 48.0f}, {-2, 2, 1.0f / 48.0f}, {-1, 2, 3.0f / 48.0f}, {0, 2,  5.0f / 48.0f}, {1, 2,  3.0f / 48.0f}, {2, 2,  1.0f / 48.0f} }
     },
     {EFilteringMethod::stucki,
-        { {1, 0,  8 / 42}, {2, 0,  4 / 42}, {-2, 1, 2 / 42}, {-1, 1, 4 / 42}, {0, 1,  8 / 42}, {1, 1,  4 / 42}, {2, 1,  2 / 42}, {-2, 2, 1 / 42}, {-1, 2, 2 / 42}, {0, 2,  4 / 42}, {1, 2,  2 / 42}, {2, 2,  1 / 42} }
+        { {1, 0,  8.0f / 42.0f}, {2, 0,  4.0f / 42.0f}, {-2, 1, 2.0f / 42.0f}, {-1, 1, 4.0f / 42.0f}, {0, 1,  8.0f / 42.0f}, {1, 1,  4.0f / 42.0f}, {2, 1,  2.0f / 42.0f}, {-2, 2, 1.0f / 42.0f}, {-1, 2, 2.0f / 42.0f}, {0, 2,  4.0f / 42.0f}, {1, 2,  2.0f / 42.0f}, {2, 2,  1.0f / 42.0f} }
     },
     {EFilteringMethod::burkes,
-        { {1, 0,  8 / 32}, {2, 0,  4 / 32}, {-2, 1, 2 / 32}, {-1, 1, 4 / 32}, {0, 1,  8 / 32}, {1, 1,  4 / 32}, {2, 1,  2 / 32} }
+        { {1, 0,  8.0f / 32.0f}, {2, 0,  4.0f / 32.0f}, {-2, 1, 2.0f / 32.0f}, {-1, 1, 4.0f / 32.0f}, {0, 1,  8.0f / 32.0f}, {1, 1,  4.0f / 32.0f}, {2, 1,  2.0f / 32.0f} }
     },
     {EFilteringMethod::sierra3,
-        { {1, 0,  5 / 32}, {2, 0,  3 / 32}, {-2, 1, 2 / 32}, {-1, 1, 4 / 32}, {0, 1,  5 / 32}, {1, 1,  4 / 32}, {2, 1,  2 / 32}, {-1, 2, 2 / 32}, {0, 2,  3 / 32}, {1, 2,  2 / 32} }
+        { {1, 0,  5.0f / 32.0f}, {2, 0,  3.0f / 32.0f}, {-2, 1, 2.0f / 32.0f}, {-1, 1, 4.0f / 32.0f}, {0, 1,  5.0f / 32.0f}, {1, 1,  4.0f / 32.0f}, {2, 1,  2.0f / 32.0f}, {-1, 2, 2.0f / 32.0f}, {0, 2,  3.0f / 32.0f}, {1, 2,  2.0f / 32.0f} }
     },
     {EFilteringMethod::sierra2,
-        { {1, 0,  4 / 16}, {2, 0,  3 / 16}, {-2, 1, 1 / 16}, {-1, 1, 2 / 16}, {0, 1,  3 / 16}, {1, 1,  2 / 16}, {2, 1,  1 / 16} }
+        { {1, 0,  4.0f / 16.0f}, {2, 0,  3.0f / 16.0f}, {-2, 1, 1.0f / 16.0f}, {-1, 1, 2.0f / 16.0f}, {0, 1,  3.0f / 16.0f}, {1, 1,  2.0f / 16.0f}, {2, 1,  1.0f / 16.0f} }
     },
     {EFilteringMethod::sierra24a,
-        { {1, 0,  2 / 4}, {-1, 1, 1 / 4}, {0, 1,  1 / 4} }
+        { {1, 0,  2.0f / 4.0f}, {-1, 1, 1.0f / 4.0f}, {0, 1,  1.0f / 4.0f} }
     },
     {EFilteringMethod::stevensonArce, 
-        { {2, 0,   32 / 200}, {-3, 1,  12 / 200}, {-1, 1,  26 / 200}, {1, 1,   30 / 200}, {3, 1,   30 / 200}, {-2, 2,  12 / 200}, {0, 2,   26 / 200}, {2, 2,   12 / 200}, {-3, 3,   5 / 200}, {-1, 3,  12 / 200}, {1, 3,   12 / 200}, {3, 3,    5 / 200} }
+        { {2, 0,   32.0f / 200.0f}, {-3, 1,  12.0f / 200.0f}, {-1, 1,  26.0f / 200.0f}, {1, 1,   30.0f / 200.0f}, {3, 1,   30.0f / 200.0f}, {-2, 2,  12.0f / 200.0f}, {0, 2,   26.0f / 200.0f}, {2, 2,   12.0f / 200.0f}, {-3, 3,   5.0f / 200.0f}, {-1, 3,  12.0f / 200.0f}, {1, 3,   12.0f / 200.0f}, {3, 3,    5.0f / 200.0f} }
     }
 }};
 
 uint8_t rgbToGrayscale(const uint8_t r, const uint8_t g, const uint8_t b);
 uint8_t rgbToGrayscale(const PIXELFORMAT_RGB& pixel);
 
-Matrix<uint8_t> convertToGrayscale(const Matrix<PIXELFORMAT_RGB> inMat);
+Matrix<uint8_t> convertToGrayscale(const Matrix<PIXELFORMAT_RGB>& inMat);
 
-Matrix<uint8_t> errorDiffusion(const Matrix<uint8_t> inMat, const float threshold = 0.5f, EFilteringMethod method = EFilteringMethod::floydSteinberg);
+void errorDiffusion(Matrix<uint8_t>& mat, const float threshold = 0.5f, EFilteringMethod method = EFilteringMethod::floydSteinberg);
 
-} // namespace filtering
+}// namespace filtering
